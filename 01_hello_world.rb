765bd2dc891c0ea58da1602384f34a7e3b7f0828
shusuke unno <seafield1979@gmail.com>
2016-09-22 22:35:53 +0900

    Rubyプログラムテスト用

---
+++ 01_hello_world.rb
@@ -0,0 +1,2 @@
+#"hello world"表示プログラム
+puts "hello world!!"
---
+++ 02_gets_str.rb
@@ -0,0 +1,5 @@
+#コマンドラインから文字列を入力するプログラム
+# 起動例: $ruby -Ku 02_gets_str.rb
+puts "文字列を入力してください"
+input_value = gets
+puts "あなたの入力した文字列は" + input_value
